f72007761ff751b43965f4016f7393744d31c269
Anda <andadeacu@yahoo.com>
2019-12-23 19:23:40 +0200

    Update with TestNG
    Update with TestNG

--- src/test/java/com/ravi/common/AppTest.java
+++ src/test/java/com/ravi/common/AppTest.java
@@ -1,8 +1,7 @@
 package com.ravi.common;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
@@ -15,24 +14,8 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+ @Test
+  public void f() {
+	  Assert.assertEquals("anda", "anda");
+  }
 }
