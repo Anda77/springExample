c222a4d0521af7765332c747df0e71a04c3e5c95
Anda <andadeacu@yahoo.com>
2020-01-03 22:49:01 +0200

    Create AppTest1.java

---
+++ src/test/java/com/anda/common/AppTest1.java
@@ -0,0 +1,22 @@
+package com.anda.common;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest1 
+ 
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+ @Test
+  public void f() {
+	  Assert.assertEquals("anda", "anda");
+  }
+}
